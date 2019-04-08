645bd2909f0d8f9e2e950d14f00fa851c8dfb827
Danilo Mezgec <mezgec.danilo@gmail.com>
2019-04-08 17:13:01 +0200

    feat: adds styled-components library and a Demo component

---
+++ src/components/Demo/Demo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+    background-color: black;
+    color: white;
+    padding: 1rem;
+`;
+
+function Demo() {
+    return (
+        <Wrapper>Demo</Wrapper>
+    )
+}
+
+export default Demo;
